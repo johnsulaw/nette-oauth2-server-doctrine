8e0578cd3d2d0dd7ea047515b7bdec075c184f4c
johnsulaw <jan.sulavik@seznam.cz>
2025-04-10 16:22:03 +0200

    setClass s argumentem nahrazeno za setFactory

--- src/NetteOAuth2ServerDoctrineExtension.php
+++ src/NetteOAuth2ServerDoctrineExtension.php
@@ -60,11 +60,11 @@ class NetteOAuth2ServerDoctrineExtension extends CompilerExtension implements IE
 		// Table mapping & Login redirection
 		Validators::assertField($config, 'tablePrefix', 'string');
 		$builder->addDefinition($this->prefix('tablePrefixSubscriber'))
-			->setClass(TablePrefixSubscriber::class, [$config['tablePrefix']])
+			->setFactory(TablePrefixSubscriber::class, [$config['tablePrefix']])
 			->addTag(EventsExtension::TAG_SUBSCRIBER);
 		Validators::assertField($config, 'loginEventPriority', 'integer');
 		$builder->addDefinition($this->prefix('loginSubscriber'))
-			->setClass(LoginSubscriber::class, ['priority' => $config['loginEventPriority']])
+			->setFactory(LoginSubscriber::class, ['priority' => $config['loginEventPriority']])
 			->addTag(EventsExtension::TAG_SUBSCRIBER);
 
 		// Common repositories
@@ -96,12 +96,12 @@ class NetteOAuth2ServerDoctrineExtension extends CompilerExtension implements IE
 
 		// Authorization & resource server
 		$authorizationServer = $builder->addDefinition($this->prefix('authorizationServer'))
-			->setClass(AuthorizationServer::class, [
+			->setFactory(AuthorizationServer::class, [
 				'privateKey' => $privateKey,
 				'encryptionKey' => $config['encryptionKey'],
 			]);
 		$builder->addDefinition($this->prefix('resourceServer'))
-			->setClass(ResourceServer::class, [
+			->setFactory(ResourceServer::class, [
 				'publicKey' => $config['publicKey'],
 			]);
 
@@ -123,7 +123,7 @@ class NetteOAuth2ServerDoctrineExtension extends CompilerExtension implements IE
 					if (!array_key_exists('authCodeTtl', $options)) {
 						$options['authCodeTtl'] = 'PT10M';
 					}
-					$definition->setClass(AuthCodeGrant::class, ['authCodeTTL' => $this->createDateIntervalStatement($options['authCodeTtl'])]);
+					$definition->setFactory(AuthCodeGrant::class, ['authCodeTTL' => $this->createDateIntervalStatement($options['authCodeTtl'])]);
 					if (array_key_exists('pkce', $options)) {
 						Validators::assertField($options, 'pkce', 'boolean');
 						if ($options['pkce']) {
@@ -138,7 +138,7 @@ class NetteOAuth2ServerDoctrineExtension extends CompilerExtension implements IE
 					if (!array_key_exists('accessTokenTtl', $options)) {
 						$options['accessTokenTtl'] = 'PT10M';
 					}
-					$definition->setClass(ImplicitGrant::class, ['accessTokenTTL' => $this->createDateIntervalStatement($options['accessTokenTtl'])]);
+					$definition->setFactory(ImplicitGrant::class, ['accessTokenTTL' => $this->createDateIntervalStatement($options['accessTokenTtl'])]);
 					break;
 				case 'password':
 					$definition->setClass(PasswordGrant::class);
@@ -170,7 +170,7 @@ class NetteOAuth2ServerDoctrineExtension extends CompilerExtension implements IE
 		Validators::assertField($config, 'approveDestination', 'string|null');
 		Validators::assertField($config, 'loginDestination', 'string|null');
 		$builder->addDefinition($this->prefix('redirectConfig'))
-			->setClass(RedirectConfig::class, [
+			->setFactory(RedirectConfig::class, [
 				'approveDestination' => $config['approveDestination'],
 				'loginDestination' => $config['loginDestination'],
 			]);
